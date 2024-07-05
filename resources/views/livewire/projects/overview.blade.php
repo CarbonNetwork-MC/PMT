c87a2f5613455db38214395f36734c5f95f192a6
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-07-06 00:47:49 +0200

    Styling fixes

--- resources/views/livewire/projects/overview.blade.php
+++ resources/views/livewire/projects/overview.blade.php
@@ -13,11 +13,11 @@
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $project->name }}</h5>
                 <div class="flex gap-x-4">
                     <div class="flex">
-                        <i class="fi fi-sr-running text-xl"></i>                  
+                        <i class="fi fi-sr-running text-xl dark:text-white"></i>                  
                         <p class="ml-2 text-gray-800 dark:text-white">{{ $project->sprints->count() }}</p>                    
                     </div>
                     <div class="flex">
-                        <i class="fi fi-sr-users-alt"></i>
+                        <i class="fi fi-sr-users-alt dark:text-white"></i>
                         <p class="ml-2 text-gray-800 dark:text-white">{{ $project->members->count() }}</p>
                     </div>
                 </div>
