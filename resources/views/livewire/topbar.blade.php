3530b9bcbedb4e13b57acfe36d02ebf36ce017e7
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-07-06 00:51:42 +0200

    Logout button fix

--- resources/views/livewire/topbar.blade.php
+++ resources/views/livewire/topbar.blade.php
@@ -26,23 +26,18 @@
     
         <div x-show="open" @click.away="open = false" class="absolute right-5 mt-2 w-48 bg-white dark:bg-gray-800 rounded-md shadow-lg z-10">
             <a href="{{ route('profile.show') }}" class="block px-2 py-2 flex items-center text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                    <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
-                </svg>              
+                <i class="fi fi-sr-user text-xl"></i>             
                 <p class="pl-2">Profile</p>
             </a>
             <a href="" class="block px-2 py-2 flex items-center text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                    <path fill-rule="evenodd" d="M9.586 2.586A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2v.089l.473.196.063-.063a2.002 2.002 0 0 1 2.828 0l1.414 1.414a2 2 0 0 1 0 2.827l-.063.064.196.473H20a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-.089l-.196.473.063.063a2.002 2.002 0 0 1 0 2.828l-1.414 1.414a2 2 0 0 1-2.828 0l-.063-.063-.473.196V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.089l-.473-.196-.063.063a2.002 2.002 0 0 1-2.828 0l-1.414-1.414a2 2 0 0 1 0-2.827l.063-.064L4.089 15H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h.09l.195-.473-.063-.063a2 2 0 0 1 0-2.828l1.414-1.414a2 2 0 0 1 2.827 0l.064.063L9 4.089V4a2 2 0 0 1 .586-1.414ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z" clip-rule="evenodd"/>
-                </svg>        
+                <i class="fi fi-ss-settings text-xl"></i>    
                 <p class="pl-2">Settings</p>
             </a>
-            <a href="{{ route('logout') }}" class="block px-2 py-2 text-sm flex items-center text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 border-t border-gray-300">
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"/>
-                </svg>
-                <p class="pl-2">Logout</p>
-            </a>
+            <form method="POST" action="{{ route('logout') }}" class="block px-2 py-2 text-sm flex items-center text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 border-t border-gray-300">
+                @csrf
+                <i class="fi fi-sr-sign-out-alt text-xl"></i>
+                <button type="submit" class="pl-2">Logout</button>
+            </form>
         </div>
     </div>
 </div>
