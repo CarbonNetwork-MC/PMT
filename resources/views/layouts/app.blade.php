ae02c72aa9c71763df38edad373fdf7b9d1a2128
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-07-08 14:59:21 +0200

    Added the Toastr JS Library

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,10 @@
         <!-- jQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
+        <!-- Toastr -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet"/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
         <!-- Styles -->
         @livewireStyles
     </head>
