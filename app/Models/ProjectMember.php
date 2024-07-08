838a12d89ff67425e8fd6cce34aef30837c9483e
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-07-08 14:58:48 +0200

    Updated the Project Member model

--- app/Models/ProjectMember.php
+++ app/Models/ProjectMember.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -13,11 +14,16 @@ class ProjectMember extends Model
     protected $fillable = [
         'project_id',
         'user_id',
-        'role',
+        'role_id',
     ];
 
     public function project(): BelongsTo
     {
         return $this->belongsTo(Project::class);
+    }
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'uuid', 'user_id');
     }
 }
