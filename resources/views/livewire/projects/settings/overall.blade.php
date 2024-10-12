5a8952ab4a7efff5e548e3f6ec6eca61d80906d8
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-10-12 23:39:07 +0200

    Project Permissions fixed on the settings page

--- resources/views/livewire/projects/settings/overall.blade.php
+++ resources/views/livewire/projects/settings/overall.blade.php
@@ -8,21 +8,33 @@
             <div class="mt-4 p-4">
                 <div class="grid grid-cols-3">
                     <div class="col-span-1">
-                        <label for="name" class="block text-sm font-medium text-gray-700 dark:text-gray-200">{{ __('settings.project_name') }}</label>
-                        <input type="text" id ="name" wire:model="name" class="w-full dark:bg-gray-900 dark:text-white border border-gray-300 dark:border-gray-700 rounded p-2 mt-2" placeholder="{{ __('settings.project_name') }}" />
+                        @if ($user->role->id !== 1)
+                            <label for="name" class="block text-sm font-medium text-gray-700 dark:text-gray-200">{{ __('settings.project_name') }}</label>
+                            <input type="text" id ="name" wire:model="name" class="w-full dark:bg-gray-900 dark:text-white border border-gray-300 dark:border-gray-700 rounded p-2 mt-2" placeholder="{{ __('settings.project_name') }}" />
+                        @else
+                            <p class="text-lg font-bold dark:text-white">{{ __('settings.project_name') }}</p>
+                            <p class="text-sm dark:text-white mt-2">{{ $name }}</p>
+                        @endif
                     </div>
                 </div>
                 <hr class="h-px my-8 bg-gray-300 border-0 dark:bg-gray-700">
                 <div class="grid grid-cols-3">
                     <div class="col-span-2">
-                        <label for="description" class="block text-sm font-medium text-gray-700 dark:text-gray-200">{{ __('settings.project_description') }}</label>
-                        <textarea id="description" rows="5" wire:model="description" class="w-full dark:bg-gray-900 dark:text-white border border-gray-300 dark:border-gray-700 rounded resize-none p-2 mt-2" placeholder="{{ __('settings.project_description') }}"></textarea>
+                        @if ($user->role->id !== 1)
+                            <label for="description" class="block text-sm font-medium text-gray-700 dark:text-gray-200">{{ __('settings.project_description') }}</label>
+                            <textarea id="description" rows="5" wire:model="description" class="w-full dark:bg-gray-900 dark:text-white border border-gray-300 dark:border-gray-700 rounded resize-none p-2 mt-2" placeholder="{{ __('settings.project_description') }}"></textarea>
+                        @else
+                            <p class="text-lg font-bold dark:text-white">{{ __('settings.project_description') }}</p>
+                            <p class="text-sm dark:text-white mt-2">{{ $description }}</p>
+                        @endif
                     </div>
                 </div>
             </div>
         </div>
-        <div class="flex justify-end bg-gray-100 dark:bg-gray-700 rounded-b-md p-4">
-            <button wire:click="save" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('settings.save') }}</button>
-        </div>
+        @if ($user->role->id !== 1)
+            <div class="flex justify-end bg-gray-100 dark:bg-gray-700 rounded-b-md p-4">
+                <button wire:click="save" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('settings.save') }}</button>
+            </div>
+        @endif
     </div>
 </div>
