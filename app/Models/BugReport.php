f9f1567e64ab9ebfe2d7ad1300a7eb8d63e0b9cc
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-10-24 19:36:15 +0200

    Bug report model

---
+++ app/Models/BugReport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class BugReport extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'page',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
