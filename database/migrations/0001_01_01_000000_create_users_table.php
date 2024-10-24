5220a07d35ccc1af295ecaecc835ceafcccb1801
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-10-24 17:40:46 +0200

    int -> integer in migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->int('role_id')->default(1);
+            $table->integer('role_id')->default(1);
             $table->string('color_hex', 6)->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
