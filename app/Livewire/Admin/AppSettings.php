9d4aabb8779e5cb55714f06211248fc77cf2c1ea
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-10-24 23:16:35 +0200

    Working app settings page

--- app/Livewire/Admin/AppSettings.php
+++ app/Livewire/Admin/AppSettings.php
@@ -3,9 +3,32 @@
 namespace App\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Setting;
 
 class AppSettings extends Component
 {
+    public $settings;
+    public $settingsValues = [];
+
+    public function mount() {
+        $this->settings = Setting::all();
+
+        foreach ($this->settings as $setting) {
+            $this->settingsValues[$setting->id] = $setting->value;
+        }
+    }
+
+    public function saveSetting($settingId) {
+        $setting = Setting::find($settingId);
+        $setting->value = $this->settingsValues[$settingId];
+        $setting->save();
+    }
+
+    /**
+     * Render the component.
+     * 
+     * @return \Illuminate\View\View
+     */
     public function render()
     {
         return view('livewire.admin.app-settings');
--- resources/views/livewire/admin/app-settings.blade.php
+++ resources/views/livewire/admin/app-settings.blade.php
@@ -1,3 +1,35 @@
-<div>
-    {{-- Be like water. --}}
+<div class="flex flex-col h-[90vh]">
+    <x-slot name="title">
+        {{ __('admin.app_settings') }}
+    </x-slot>
+
+    <div class="w-full bg-white dark:bg-gray-800 shadow-md rounded-lg p-4">
+        <p class="text-lg font-bold dark:text-white">{{ __('admin.app_settings') }}</p>
+        <div class="w-full mt-8">
+            @foreach ($settings as $setting)
+                <div class="grid grid-cols-3">
+                    <div class="col-span-1 flex items-center">
+                        <p class="text-sm font-bold uppercase dark:text-white">{{ $setting->key }}</p>
+                    </div>
+                    @switch($setting->type)
+                        @case('string')
+                            <div class="col-span-1">
+                                <input type="text" 
+                                    wire:model.lazy="settingsValues.{{ $setting->id }}"
+                                    wire:keydown.enter="saveSetting('{{ $setting->id }}')"
+                                    class="w-full p-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:text-white">
+                            </div>
+                            @break
+                        @case('text')
+                            <div class="col-span-2">
+                                <textarea wire:model.lazy="settingsValues.{{ $setting->id }}"
+                                    wire:keydown.enter="saveSetting('{{ $setting->id }}')"
+                                    class="w-full p-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:text-white"></textarea>
+                            </div>
+                            @break
+                    @endswitch
+                </div>
+            @endforeach
+        </div>
+    </div>
 </div>
