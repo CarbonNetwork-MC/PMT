486d83b6cc3402d5b141712d51f3037ef63d85b2
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-10-24 22:45:03 +0200

    Updated settings table

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -9,5 +9,9 @@ class Setting extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['key', 'value'];
+    protected $fillable = [
+        'key', 
+        'value',
+        'type'
+    ];
 }
--- database/migrations/2024_10_24_195149_create_settings_table.php
+++ database/migrations/2024_10_24_195149_create_settings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('key')->unique();
             $table->text('value')->nullable();
+            $table->string('type')->default('text');
             $table->timestamps();
         });
     }
