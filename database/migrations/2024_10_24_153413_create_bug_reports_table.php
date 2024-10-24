194b0f3ae4ddfdca9a147279631283ef0e290b17
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2024-10-24 19:25:46 +0200

    Bug reports migration

---
+++ database/migrations/2024_10_24_153413_create_bug_reports_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bug_reports', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->string('page');
+            $table->enum('status', ['open', 'closed']);
+            $table->char('user_id', 36);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bug_reports');
+    }
+};
