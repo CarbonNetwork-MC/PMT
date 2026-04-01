819a4c68ff95dfa3d8925c25a2c2093152059626
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-04-01 20:18:33 +0200

    feat: show message when the user has no projects

--- resources/views/livewire/projects/projects.blade.php
+++ resources/views/livewire/projects/projects.blade.php
@@ -19,7 +19,12 @@
         @forelse ($projects as $project)
             <x-project.card :project="$project" />
         @empty
-
+            <div class="col-span-full">
+                <div class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-200">No Projects Found</h2>
+                    <p class="mt-2 text-gray-600 dark:text-gray-400">You haven't created or joined any projects yet. Start by creating a new project or joining an existing one.</p>
+                </div>
+            </div>
         @endforelse
     </div>
 </div>
