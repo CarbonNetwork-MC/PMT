aefa0b3052e437089007ee2ba001dff06df81bb2
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-04-01 22:41:49 +0200

    chore: renamed component

--- resources/views/livewire/projects/projects.blade.php
+++ resources/views/livewire/projects/projects.blade.php
@@ -38,7 +38,7 @@
                     : 0;
                 $memberCount = $project->owner ? $memberCount + 1 : $memberCount;
             @endphp
-            <x-project.card :project="$project" :memberCount="$memberCount" />
+            <x-project.project-card :project="$project" :memberCount="$memberCount" />
         @empty
             <div class="col-span-full">
                 <div class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
