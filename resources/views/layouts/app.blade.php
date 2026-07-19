a1859460d4a7d42e39d95666f58fbbca8657e9ac
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-07-19 20:46:23 +0200

    chore: removed the default laravel metadata title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
-
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
