4c6a21ac9b6a039aa5fb7ce54806b9362f522f43
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-07-30 15:04:31 +0200

    fix: allow image uploads on deployments that go through cloudflare or other proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
+    ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'locale' => \App\Http\Middleware\SetLocale::class,
 
